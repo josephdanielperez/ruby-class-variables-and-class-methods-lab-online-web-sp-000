d8f4930d29f330a7b6d800501aec44b5833cc9ba
Joseph Perez <hello@josephdanielperez.com>
2020-07-21 07:00:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,11 +5,8 @@ attr_reader
 
 @@song_count = 0
 
-  def initialize(name, artist, genre)
+  def initialize
     @@song_count += 1
-    @name = name
-    @artist = artist
-    @genre = genre
   end
 
   def self.count
